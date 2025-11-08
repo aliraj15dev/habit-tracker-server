1f6b0927496928c367946a74f01e583d30e45bbd
AliRaj <aliraj15dev@gmail.com>
2025-11-08 19:30:49 +0600

    basic structure made for the project

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Habit tracker Server is Working");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Habit tracker is running on port: ${port}`);
+});
