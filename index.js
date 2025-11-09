e2b891e516d2962d9500cbf11e31fa0cff057aa3
AliRaj <aliraj15dev@gmail.com>
2025-11-09 08:18:13 +0600

    successfully connect mongodb and ping check success

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,11 +9,36 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = "mongodb+srv://userHabitsDB:oSWfyZI5c5Y3WAC1@cluster0.7suxelv.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Habit tracker Server is Working");
 });
 
+async function run() {
+  try {
+    await client.connect();
 
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+  finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Habit tracker is running on port: ${port}`);
